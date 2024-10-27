38307421538e74f8a5b9c4a6a04078175dd152d7
Opomomlslsla <opomomlslsla@gmail.com>
2024-10-28 00:54:14 +0300

    some codestyle fixes

--- Aplication/Services/OrderProcessor.cs
+++ Aplication/Services/OrderProcessor.cs
@@ -16,32 +16,35 @@ public class OrderProcessor(IOptions<StorageOptions> options, IRepository<Distri
     private readonly IRepository<District> _districtRepository = districtRepository;
     private readonly IRepository<Order> _orderRepository = orderRepository;
     private readonly IRepository<FilterResult> _FilterResultRepository= filterResultRepo;
-    public async Task<OrderFIlterRequestResult> GetByTimeStampAsync(OrderFilterRequest request)
+    private readonly StorageOptions _options = options.Value;
+    public async Task<OrderFIlterRequestResult> GetByTimeStampAsync(OrderFilterRequest requestData)
     {
-        var district = await _districtRepository.GetOneByAsync(x => x.Name == request.DistrictName);
+        var district = await _districtRepository.GetOneByAsync(x => x.Name == requestData.DistrictName);
         if (district == null) { throw new ArgumentException("District not found"); }
-        var orders = await _orderRepository.GetByAsync(x => x.DeliveryDistrict.Name == district.Name && x.DeliveryDate > request.StartTime && x.DeliveryDate < request.EndTime);
-        await SaveResultAsync(orders, request.StartTime, request.EndTime, district);
-        var result  = new OrderFIlterRequestResult() { StartTime = request.StartTime, EndTime = request.EndTime, Orders = orders.Adapt<List<OrderData>>(), District = district };
+        var orders = await _orderRepository.GetByAsync(x => x.DeliveryDistrict.Name == district.Name && x.DeliveryDate > requestData.StartTime && x.DeliveryDate < requestData.EndTime);
+        if (_options.UseDatabase)
+            await SaveResultToDbAsync(orders, requestData.StartTime, requestData.EndTime, district);
+        if (_options.UseFile)
+            await SaveResultToFileAsync(orders, requestData.StartTime, requestData.EndTime, district);
+        var result  = new OrderFIlterRequestResult() { StartTime = requestData.StartTime, EndTime = requestData.EndTime, Orders = orders.Adapt<List<OrderData>>(), District = district };
         return result;
     }
 
-    private async Task SaveResultAsync(ICollection<Order> result, DateTime start, DateTime end, District district )
+    private async Task SaveResultToDbAsync(ICollection<Order> orders, DateTime start, DateTime end, District district )
     {
-        if (options.Value.UseDatabase)
+        var filterResult = new FilterResult() { StartTime = start, EndTime = end, ResultData = orders.Adapt<List<OrderData>>(), District = district };
+        await _FilterResultRepository.AddAsync(filterResult);
+        await _FilterResultRepository.SaveChangesAsync();
+    }
+
+    private async Task SaveResultToFileAsync(ICollection<Order> orders, DateTime start, DateTime end, District district)
+    {
+        var storageFile = $"{options.Value.FilePath}\\Result_{DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")}.txt";
+        var filterResult = new FilterResult() { StartTime = start, EndTime = end, ResultData = orders.Adapt<List<OrderData>>(), District = district };
+        using (StreamWriter writer = new StreamWriter(storageFile, true, Encoding.UTF8))
         {
-            var filterResult = new FilterResult() { StartTime = start, EndTime = end, ResultData = result.Adapt<List<OrderData>>(), District = district };
-            await _FilterResultRepository.AddAsync(filterResult);
-            await _FilterResultRepository.SaveChangesAsync();
-        }
-        if (options.Value.UseFile) 
-        {
-            var storageFile =$"{options.Value.FilePath}\\Result_{DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")}.txt";
-            var filterResult = new FilterResult() { StartTime = start, EndTime = end, ResultData = result.Adapt<List<OrderData>>(), District = district };
-            using (StreamWriter writer = new StreamWriter(storageFile,true, Encoding.UTF8))
-            {
-                await writer.WriteLineAsync(filterResult.ToString());
-            }
+            await writer.WriteLineAsync(filterResult.ToString());
         }
     }
 }
+
--- Domain/Interfaces/ILogger.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Interfaces;
-
-public interface ILogger
-{
-    void LogInfo(string message);
-    void LogWarning(string message);
-    void LogError(string message);
-    void LogFatal(string message);
-}
